00df140ffbe8beb0373f8326a9cbaab7d4bb9795
Daniel Kigozi <kdanalex69@gmail.com>
2020-10-28 14:58:34 +0300

    commit

--- dansite/settings.py
+++ dansite/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 
     'billing',
